6107e0dced4b479dbe5deb27f1c79b5a2fbec1ba
Lynda Santiago <lyntechi@gmail.com>
2020-09-09 13:12:00 -0400

    completed up and down functions

--- migrations/20200909130324_cars.js
+++ migrations/20200909130324_cars.js
@@ -1,12 +1,18 @@
-
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema.createTable("cars", (table) => {
+    table.increments("id");
+    table.text("VIN").notNull().unique();
+    table.text("make").notNull();
+    table.text("model").notNull();
+    table.integer("mileage").notNull();
+    table.text("transmissionType");
+    table.text("titleStatus");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+    await knex.schema.dropTableIfExists("cars")
 };
-
 
 // The critical information for each car is the VIN, make, model, and mileage.
 // They also track transmission type and status of the title (clean, salvage, etc.),
